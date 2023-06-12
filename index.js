918e2f48d808e65ad143ed921940ce2ebcdd81d9
Scott <twistingplotlines@gmail.com>
2023-06-12 18:11:05 -0400

    updated path

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // private paths, answer/clue data source and 
-const CRYPTOCROSS_SOURCE_JSON_PATH = "./_data/cryptocross/cryptocross.json";
+const CRYPTOCROSS_SOURCE_JSON_PATH = "././_data/cryptocross/cryptocross.json";
 const CRYPTOCROSS_INDEX_JSON_PATH = "./_data/cryptocross/index.json";
 
 // public path, where all the cryptocross json files reside
@@ -40,7 +40,7 @@ try {
   // console.log(`contents length: ${contents.length}`);
   console.log(`filepath: ${CRYPTOCROSS_OUTPUT_FOLDER_PATH }${filename}`);
 
-  
+
   // write the current puzzle into two locations, , and also overwrite /data/cryptocross/cryptocross.json
  
   // 1. the public cryptocross folder
